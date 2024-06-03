2f76c4679335301b2eda43bb6a5be5c9b261cc2b
Houang869 <48252530+Houangnt@users.noreply.github.com>
2024-06-03 16:09:31 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,43 @@
+
+
+import requests
+
+# ====================== Public image ====================== #
+
+# Saving txt file
+resp = requests.get('http://0.0.0.0:3000/predict?source=https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/assets/zidane.jpg&save_txt=T',
+                    verify=False)
+print(resp.content)
+
+# Without save txt file, just labeling the image
+resp = requests.get('http://0.0.0.0:3000/predict?source=https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/assets/zidane.jpg',
+                    verify=False)
+print(resp.content)
+
+# You can also copy and paste the following url in your browser
+'http://0.0.0.0:3000/predict?source=https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/assets/zidane.jpg'
+
+
+# ====================== Public video ====================== #
+# (Youtube or any public server). It is not ready (yet) to return all frames labeled while using save_txt=T. So, don't try it!
+
+resp = requests.get('http://0.0.0.0:3000/predict?source=https://www.youtube.com/watch?v=MNn9qKG2UFI',
+                    verify=False)
+
+# You can also copy and paste the following url in your browser
+'http://0.0.0.0:5000/predict?source=https://www.youtube.com/watch?v=MNn9qKG2UFI'
+
+# ====================== Send local file ==================== #
+
+
+url = 'http://0.0.0.0:3000/predict'
+file_path = 'data/images/bus.jpg'
+
+params = {
+    'save_txt': 'T'
+}
+
+with open(file_path, "rb") as f:
+    response = requests.post(url, files={"myfile": f}, data=params, verify=False)
+
+print(response.content)
